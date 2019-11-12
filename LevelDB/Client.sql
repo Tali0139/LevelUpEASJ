7a816de027367c12a758d92d7399a90aa0cd3970
Konrad Schantz Kirkebjerg <konr0693@edu.easj.dk>
2019-11-12 09:32:50 +0100

    Client Table oprettet

---
+++ LevelDB/Client.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Client]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Weight] DECIMAL NOT NULL, 
+    [Gender] NCHAR(10) NOT NULL, 
+    [FatPercent] DECIMAL NOT NULL, 
+    [ClientXp] INT NOT NULL, 
+    [ClientLevel] INT NOT NULL, 
+    [Height] INT NOT NULL
+)
