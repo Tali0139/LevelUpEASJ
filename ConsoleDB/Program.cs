98c3824b6e9d6c288088b91235f9f814712349f7
Konrad Schantz Kirkebjerg <konr0693@edu.easj.dk>
2019-11-15 09:04:30 +0100

    Database

---
+++ ConsoleDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
