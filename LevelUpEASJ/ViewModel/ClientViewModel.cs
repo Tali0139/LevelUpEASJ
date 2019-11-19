2940fa7ce08d14d9d30c5ed278886dfe2b891f93
Talia Damary <tali0139@edu.easj.dk>
2019-11-19 11:45:00 +0100

    Har lavet RelayCommand, og oprettet to cs. i Viewmodel med INotifyPropertyChanged - Talia

---
+++ LevelUpEASJ/ViewModel/ClientViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using LevelUpEASJ.Annotations;
+
+namespace LevelUpEASJ.ViewModel
+{
+    class ClientViewModel:INotifyPropertyChanged
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
