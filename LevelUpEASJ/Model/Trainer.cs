8c7931c0a9f477e0c8a22f9bfee1599b9522216a
Omar Khalil Jaber <omar1623@edu.easj.dk>
2019-11-21 13:54:12 +0100

    Model_ User+ Model_Client + Model_ Trainer lavet.

---
+++ LevelUpEASJ/Model/Trainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelUpEASJ.Model
+{
+   public class Trainer : User
+    {
+
+        private int _yearsOfExperience;
+
+        public Trainer(int id, string firstName, string lastName, string username, string password, int yearsOfExperience) : base(id, firstName, lastName, username, password)
+
+        {
+            _yearsOfExperience = yearsOfExperience;
+         
+        }
+
+        public int YearsOfExperience
+        {
+            get { return _yearsOfExperience; }
+            set { _yearsOfExperience = value; }
+        }
+
+
+    }
+}
