a2dc1b3240b8abaf4f0855eebd5d56cfe9dd7048
Oliver Lasse Eierstrand <oliv1115@edu.easj.dk>
2019-11-25 14:03:05 +0100

    Startet på listview side

--- LevelUpEASJ/App.xaml.cs
+++ LevelUpEASJ/App.xaml.cs
@@ -67,7 +67,7 @@ namespace LevelUpEASJ
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(ClientPage), e.Arguments);
+                    rootFrame.Navigate(typeof(TrainerClientView), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
