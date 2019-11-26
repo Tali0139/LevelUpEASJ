42c934956bdd63dc15fd685b328d92e5bb99e42f
Tali0139 <47971571+Tali0139@users.noreply.github.com>
2019-11-26 10:05:02 +0100

    Delete ClientGifts.sql

--- LevelUpDB/dbo/Tables/ClientGifts.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ClientGifts] (
-    [CGId]   INT NOT NULL,
-    [Id]     INT NULL,
-    [GiftId] INT NULL,
-    PRIMARY KEY CLUSTERED ([CGId] ASC),
-    CONSTRAINT [FK_ClientGifts_achievementGifts (GiftId)] FOREIGN KEY ([GiftId]) REFERENCES [dbo].[achievementGifts] ([GiftItemId]),
-    CONSTRAINT [FK_ClientGifts_Client] FOREIGN KEY ([Id]) REFERENCES [dbo].[Client] ([Id])
-);
-
