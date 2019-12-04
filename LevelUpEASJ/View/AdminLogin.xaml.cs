6eb0b2535e55ed6863ca46fc1d07eeb7042fb3b7
Oliver Lasse Eierstrand <oliv1115@edu.easj.dk>
2019-12-04 10:42:18 +0100

    :)

--- LevelUpEASJ/View/AdminLogin.xaml.cs
+++ LevelUpEASJ/View/AdminLogin.xaml.cs
@@ -20,26 +20,21 @@ namespace LevelUpEASJ.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Login : Page
+    public sealed partial class AdminLogin : Page
     {
-        public Login()
+        public AdminLogin()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1 (object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(ClientPage));
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(CreateClient));
+            this.Frame.Navigate(typeof(TrainerPage));
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(AdminLogin));
+            this.Frame.Navigate(typeof(Login));
         }
     }
 }
